aec1277c20a24afd59d143479e15e26b22c86356
Mark Lehr <mark.lehr@rcc.edu>
2021-07-15 21:57:48 -0700

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

--- Class/SavingsFunction_AllConceptsChap6/main.cpp
+++ Class/SavingsFunction_AllConceptsChap6/main.cpp
@@ -21,6 +21,10 @@ float save1(float,float,int);//Using the Power Function
 float save2(float,float,int);//Using the Exponential-Log Function
 float save3(float,float,int);//Using For-Loop
 float save4(float,float,int);//Using Recursion
+float save4(float,float,float);//Illustrate Overloading/Same Function Name
+float save6(float,float,int=12);//Illustrate a Defaulted Parameter
+void  save7(float&,float,int);//Pass by reference
+int   save8(float&,float,int);//Pass by reference and Static Variable
 
 //Code Begins Execution Here with function main
 int main(int argc, char** argv) {
@@ -41,12 +45,52 @@ int main(int argc, char** argv) {
     cout<<"The number of compounding periods = "
             <<static_cast<int>(numCmpd)<<" years"<<endl;
     cout<<"The interest rate = "<<intRate*PERCENT<<"%"<<endl;
-    cout<<"Savings - Power Function     = $"<<save1(pv,intRate,numCmpd)<<endl;
-    cout<<"Savings - Exp/Log Function   = $"<<save2(pv,intRate,numCmpd)<<endl;
-    cout<<"Savings - For-Loop Function  = $"<<save3(pv,intRate,numCmpd)<<endl;
-    cout<<"Savings - Recursive Function = $"<<save4(pv,intRate,numCmpd)<<endl;
-
+    cout<<"Savings - Power Function       = $"<<save1(pv,intRate,numCmpd)<<endl;
+    cout<<"Savings - Exp/Log Function     = $"<<save2(pv,intRate,numCmpd)<<endl;
+    cout<<"Savings - For-Loop Function    = $"<<save3(pv,intRate,numCmpd)<<endl;
+    cout<<"Savings - Recursive Function   = $"<<save4(pv,intRate,numCmpd)<<endl;
+    cout<<"Savings - Overloaded Function  = $"
+            <<save4(pv,intRate,static_cast<float>(numCmpd))<<endl;
+    cout<<"Savings - Defaulted Parameter  = $"<<save6(pv,intRate)<<endl;
+    float fv=pv;
+    save7(fv,intRate,numCmpd);
+    cout<<"Savings - Pass by Reference    = $"<<fv<<endl;
+    for(int i=1;i<=4;i++){
+        fv=pv;
+        save8(fv,intRate,numCmpd);
+    }
+    fv=pv;
+    int howMany=save8(fv,intRate,numCmpd);
+    cout<<"Savings - Static Variable      = $"<<fv<<endl;
+    cout<<"The static function was called "<<howMany<<" times"<<endl;
+    
     return 0;
+}
+
+int save8(float &p_fv,float i,int n){
+    static int count=0;
+    for(int year=1;year<=n;year++){
+        p_fv*=(1+i);
+    }
+    return ++count;
+}
+
+void save7(float &p_fv,float i,int n){
+    for(int year=1;year<=n;year++){
+        p_fv*=(1+i);
+    }
+}
+
+float save6(float p,float i,int n){
+    for(int year=1;year<=n;year++){
+        p*=(1+i);
+    }
+    return p;
+}
+
+float save4(float p,float i,float n){
+    if(n<=0)return p;
+    return save4(p,i,n-1)*(1+i);
 }
 
 float save4(float p,float i,int n){
